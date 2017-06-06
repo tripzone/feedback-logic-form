045a7a6e385e3af6448fd3b403ef9e27a3f53c19
Kasra Zahir <kasrazahir@gmail.com>
2017-06-06 14:34:13 -0400

    selection working

--- src/App.js
+++ src/App.js
@@ -1,36 +1,32 @@
 import React, { Component } from 'react';
 import {observable} from 'mobx';
 import {observer} from 'mobx-react';
-import logo from './logo.svg';
 import './App.css';
 
 const testData = [
-	{name: 'ali', id:234, pic:'ali.jpg'},
-	{name: 'mamad', id:23, pic:'mamad.jpg'}
+	{name: 'ali', id:1, pic:'pics/35.jpg'},
+	{name: 'mamad', id:2, pic:'pics/39.jpg'},
+	{name: 'mamad', id:3, pic:'pics/55.jpg'},
 ]
 
 const fetchUsers = new Promise ((resolve, reject) => {
-	setTimeout(	() => {resolve(testData)}, 2000)
+	setTimeout(	() => {resolve(testData)}, 200)
 });
-
 class User {
+	@observable selected = false;
+
 	constructor(id, name, pic) {
 		this.id = id;
 		this.name = name;
 		this.pic = pic;
 	}
 }
-
 const appState = observable({
 	users: observable({
 		loaded: false,
 		data: [],
 	})
 })
-appState.loadUser = function(x) {
-
-	// this.users.data.push(x);
-}
 appState.getAllUsers = function() {
 	fetchUsers.then(x=> {
 		console.log(x)
@@ -40,28 +36,40 @@ appState.getAllUsers = function() {
 		this.users.loaded = true;
 	})
 }
+appState.selectUser = function(id) {
+	const userIndex = this.users.data.findIndex(q => q.id === id);
+	this.users.data[userIndex].selected = !this.users.data[userIndex].selected;
+}
 
 @observer class App extends Component {
-	loadUser = (x) => {
-		appState.loadUser(x)
+	clickAction = (x) => {
+		appState.getAllUsers()
 	}
-
+	selectItem = (x) =>{
+		appState.selectUser(x);
+	}
 	componentWillMount(){
 		appState.getAllUsers();
 	}
+
+	usersDisplay() {
+		return appState.users.data.map(x=> {
+			return (
+				<div onClick={()=>this.selectItem(x.id)} className={x.selected ? 'user-selected' : 'user-notselected'}>
+					<img src={x.pic}></img>
+					{x.name}
+				</div>
+			)
+		})
+	}
+
 	render() {
 		return (
-			<div className="App">
-				<div className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-					{
-						console.log(appState.users.loaded)
-					}
-					{
-						appState.users.loaded ? appState.users.data.map(x=> {return <li>{x.name}</li>}) : 'loading...'
-					}
-				</div>
-				<button onClick={() => this.loadUser('tom')}>INCREASE</button>
+			<div>
+				{
+					appState.users.loaded ? this.usersDisplay() : 'loading...'
+				}
+				<button onClick={() => this.clickAction('tom')}>INCREASE</button>
 			</div>
 		);
 	}
