32200cf44befb0700f77156f922f7a5a7f88bf5b
Kasra Zahir <kasrazahir@gmail.com>
2017-06-14 23:50:36 -0400

    prod mod

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import fetch from 'isomorphic-fetch';
 import classNames from 'classnames';
 import './App.css';
 
-const serverLink = 'http://52.55.4.4:443';
+const serverLink = 'https://kookico.com';
 const picPath = serverLink+'/public/'
 
 const fetchUsers = (apiLink) => {
